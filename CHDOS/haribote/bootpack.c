f122f9fb0fd7afc70d488414018016f75f3d9284
StungYep <1139488642.@qq.com>
2022-04-13 15:50:51 +0800

    新增日志窗口

--- CHDOS/haribote/bootpack.c
+++ CHDOS/haribote/bootpack.c
@@ -1,5 +1,4 @@
 /* bootpack */
-
 #include "bootpack.h"
 #include <stdio.h>
 
@@ -47,7 +46,7 @@ void HariMain(void)
 
 	int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
 	int j, x, y, mmx = -1, mmy = -1, mmx2 = 0;
-	struct SHEET *sht = 0, *key_win, *sht2;
+	struct SHEET *sht = 0, *key_win, *sht2, *sht_info;      
 	int *fat;
 	unsigned char *nihongo;
 	struct FILEINFO *finfo;
@@ -94,12 +93,18 @@ void HariMain(void)
 	mx = (binfo->scrnx - 16) / 2; /* 计算坐标使其位于画面中央 */
 	my = (binfo->scrny - 28 - 16) / 2;
 
+	/* sht_infomation */
+	sht_info = open_shtinfo(shtctl, memtotal);
+
+	// 固定已有窗口的位置
 	sheet_slide(sht_back,  0,  0);
 	sheet_slide(key_win,   32, 4);
 	sheet_slide(sht_mouse, mx, my);
+	sheet_slide(sht_info, 64, 8);
 	sheet_updown(sht_back,  0);
-	sheet_updown(key_win,   1);
-	sheet_updown(sht_mouse, 2);
+	sheet_updown(sht_info,  1);
+	sheet_updown(key_win,   2);
+	sheet_updown(sht_mouse, 3);
 	keywin_on(key_win);
 
 	/* 为了避免和键盘当前状态冲突，在一开始先进行设置 */
@@ -389,6 +394,18 @@ struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal)
 	make_textbox8(sht, 8, 28, 496, 302, COL8_000000);
 	sht->task = open_constask(sht, memtotal);
 	sht->flags |= 0x20;	/*有光标*/
+	return sht;
+}
+
+struct SHEET *open_shtinfo(struct SHTCTL *shtctl, unsigned int memtotal)
+{
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+	struct SHEET *sht = sheet_alloc(shtctl);
+	unsigned char *buf = (unsigned char *) memman_alloc_4k(memman, 512 * 330);
+	sheet_setbuf(sht, buf, 512, 330, -1); /*无透明色*/
+	make_window8(buf, 512, 330, "infomation", 0);
+	make_textbox8(sht, 8, 28, 496, 302, COL8_000000);
+	sht->task = open_constask(sht, memtotal);
 	return sht;
 }
 
